b4db3bebbc76bbe7f620c22b7c9ae0540d66fe36
Sergey Nagornykh <serega1905@ukr.net>
2018-02-23 01:32:43 +0200

    upd other and add validation

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,4 @@ import './partials/_toggleContent';
 import './partials/_popup';
 import './partials/_svgPolyfil';
 import './partials/_animations';
+import './partials/_validation';
---
+++ src/js/partials/_validation.js
@@ -0,0 +1,19 @@
+import 'jquery-validation';
+
+const form = $('.js-contact-form');
+
+form.validate({
+  rules: {
+    name_contact: {
+      required: true
+    },
+    email_contact: {
+      required: true,
+      email: true
+    },
+    phone_contact: {
+      required: true,
+      digits: true
+    },
+  }
+});
