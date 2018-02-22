8a4274b8a1972049ce6fd82f43bd11cef8c10ac8
Sergey Nagornykh <serega1905@ukr.net>
2018-02-22 13:50:41 +0200

    fixed header

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import './partials/_scrollTo';
 import './partials/_particles';
 import './partials/_toggleContent';
 import './partials/_popup';
+import './partials/_header';
 import {WOW} from 'wowjs';
 
 new WOW().init();
---
+++ src/js/partials/_header.js
@@ -0,0 +1,53 @@
+import $ from 'jquery';
+
+const WIN = $(window);
+const HEADER = $('.js-header');
+const isScrolling = () => $(window).scrollTop() > 40 ? true : false;
+let scrollFlag = true;
+
+const addAnimationFromHeader = () => {
+  scrollFlag = false;
+  HEADER.addClass('is-animate');
+};
+
+const removeAnimationFromHeader = () => {
+  scrollFlag = true;
+  HEADER.removeClass('is-animate');
+};
+
+WIN.on('load', () => {
+  if(isScrolling()) { 
+    addAnimationFromHeader();
+  }
+});
+
+WIN.on('scroll', () => {
+  if(isScrolling() && scrollFlag) {
+    addAnimationFromHeader();
+  }
+  if(!isScrolling() && !scrollFlag) {
+    removeAnimationFromHeader();
+  }
+  checkSection();
+});
+
+
+let checkSection = () => {
+  $('[data-section]').each(function() {
+    let topSection = $(this).offset().top - 100;
+    let bottomSection = topSection + $(this).height();
+    let scrollTop = WIN.scrollTop();
+    let id = $(this).data('section');
+    let links = $('.nav__link');
+    let currentLink = links.filter('[href="#' + id +'"]');
+
+    if(topSection < scrollTop && bottomSection > scrollTop) {
+	  
+	  links.removeClass('is-active');
+	  currentLink.addClass('is-active');
+	  window.location.hash = id;
+    }
+  });
+};
+
+// showSection(window.location.hash, false);
