87d163e8d49ba0d1380f972098e9d56cea13d731
Sergey Nagornykh <serega1905@ukr.net>
2018-04-23 00:07:45 +0300

    header mixin, new pages, upd mobile logo, adaptive, scroll on load page

--- src/js/partials/_scrollTo.js
+++ src/js/partials/_scrollTo.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 const htmlBody  = $('html, body');
 const duration  = 1200;
+const hash = window.location.hash;
 
 $('.js-nav-link, .js-btn-scroll').on('click', (e) => {
   const anchor = $(e.currentTarget).attr('href').slice(1);
@@ -9,6 +10,15 @@ $('.js-nav-link, .js-btn-scroll').on('click', (e) => {
 
   scrollToSection(section, true);
 });
+
+
+
+if(hash !== '') {
+  let section = $('[data-section="'+ hash.slice(1) +'"]');
+
+  console.log(section, hash);
+  scrollToSection(section, true);
+}
 
 
 function scrollToSection(section, animate) {
